f9e35bb630f64e6679cec467d00c264ed01ceec7
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-12-26 18:40:01 +0530

    Added randomized hover colours

--- index.js
+++ index.js
@@ -37,13 +37,16 @@ function newGrid() {
   }
 
   container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  let v1 = Math.floor(Math.random() * 255);
+  let v2 = Math.floor(Math.random() * 255);
+  let v3 = Math.floor(Math.random() * 255);
   for (i = 0; i < value; i++) {
     for (j = 0; j < value; j++) {
       let sqr = document.createElement("div");
       sqr.classList.add("sqrdiv");
       container.appendChild(sqr);
       sqr.addEventListener("mouseenter", function () {
-        sqr.style.backgroundColor = "blue";
+        sqr.style.backgroundColor = `rgb(${v1}, ${v2}, ${v3})`;
       });
       /*sqr.addEventListener("mouseleave", function () {
         sqr.style.backgroundColor = "rgb(29, 175, 185)";
