824d2c10367fd513292f72095e87d7ee174bee3d
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-12-26 18:35:12 +0530

    A simple etch-a-sketch webpage that allows you to draw on a grid with your mouse. The grid size can be changed by entering a number in the input box and clicking the "New Grid" button.

---
+++ index.js
@@ -0,0 +1,54 @@
+let container = document.createElement("div");
+container.classList.add("container");
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let sqr = document.createElement("div");
+    sqr.classList.add("sqrdiv");
+    container.appendChild(sqr);
+    sqr.addEventListener("mouseenter", function () {
+      sqr.style.backgroundColor = "blue";
+    });
+    sqr.addEventListener("mouseleave", function () {
+      sqr.style.backgroundColor = "rgb(29, 175, 185)";
+    });
+  }
+}
+
+let button = document.createElement("button");
+button.textContent = "New Grid";
+
+document.body.appendChild(button);
+
+document.body.appendChild(container);
+
+button.addEventListener("click", newGrid);
+
+function newGrid() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+
+  let value = prompt("Enter a number less than 100");
+  if (value > 100) {
+    alert("Please enter a number less than 100");
+    return;
+  }
+
+  container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  for (i = 0; i < value; i++) {
+    for (j = 0; j < value; j++) {
+      let sqr = document.createElement("div");
+      sqr.classList.add("sqrdiv");
+      container.appendChild(sqr);
+      sqr.addEventListener("mouseenter", function () {
+        sqr.style.backgroundColor = "blue";
+      });
+      sqr.addEventListener("mouseleave", function () {
+        sqr.style.backgroundColor = "rgb(29, 175, 185)";
+      });
+    }
+  }
+  document.body.appendChild(container);
+}
